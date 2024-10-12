ab42c85fa95a08417591f7cf6a887221d2bd0cd7
Marcusdev97 <marcuschin.flp@gmail.com>
2024-10-13 01:37:27 +0800

    change baseURL

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,12 +2,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: process.env.NODE_ENV === 'production' ? 'https://myeasyrenthub.com' : 'http://localhost:3000',
 });
 
 export const fetchProperties = async () => {
   try {
-    const response = await api.get('/properties');
+    const response = await api.get('/api/properties');
     return response.data;
   } catch (error) {
     console.error('Error fetching properties:', error);
