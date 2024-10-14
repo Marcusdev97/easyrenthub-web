a542eda921e54702ead2f568fad6745a8edcccff
Marcusdev97 <marcuschin.flp@gmail.com>
2024-10-14 11:29:04 +0800

    Add debug for console.log environment

--- api/properties.js
+++ api/properties.js
@@ -35,6 +35,14 @@ module.exports = async (req, res) => {
   // Run CORS middleware
   await runMiddleware(req, res, cors(corsOptions));
 
+  console.log('Request received at /api/properties');
+  console.log('Environment Variables:', {
+    DB_HOST: process.env.DB_HOST,
+    DB_USER: process.env.DB_USER,
+    DB_NAME: process.env.DB_NAME,
+    DB_PORT: process.env.DB_PORT,
+  });
+
   if (req.method === 'GET') {
     try {
       const connection = await mysql.createConnection({
